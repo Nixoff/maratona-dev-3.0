235be65ac17f8898e02aff413cf8e9ae6666d9e4
Nixoff <gabadev48@protonmail.com>
2020-02-20 06:33:08 -0300

    finished front end ⌛️

---
+++ src/js/scripts.js
@@ -0,0 +1,7 @@
+document.querySelector("Header button").addEventListener("click", function() {
+  document.querySelector(".form").classList.toggle("hide");
+  document.querySelector(".logo").classList.toggle("animated");
+  document.querySelector(".logo").classList.toggle("bounce");
+});
+document.querySelector(".heart").classList.toggle("animated");
+document.querySelector(".heart").classList.toggle("flash");
